4df94dc7f144bc731f886ea8e4061438321110e5
Virobeast <shane@onsite2u.com.au>
2016-01-10 03:47:34 +0800

    Remove Pause

--- Bat-Originals/64clean.bat
+++ Bat-Originals/64clean.bat
@@ -1,4 +1,3 @@
 REG ADD HKCU\Software\TeamViewer /v HostInfoDialog /t REG_DWORD /d 0 /f
 REG ADD HKU\.DEFAULT\Software\TeamViewer /v HostInfoDialog /t REG_DWORD /d 0 /f
 regedit /s TeamViewer_Settings.reg
-pause
